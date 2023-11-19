a3189aad32c4f2eb7a442a35ae8261034afa48d5
TodTheSushiMonster <nconzetti@gmail.com>
2023-11-19 14:39:18 -0800

    Scene Management Code
    Added code to scene management that checks if a scene exists, and loads it if does

--- Assets/Scripts/App/SceneManager/SceneManager.cs
+++ Assets/Scripts/App/SceneManager/SceneManager.cs
@@ -2,6 +2,14 @@ using UnityEngine;
 
 namespace App.Scene {
     public class SceneManager : MonoBehaviour{
-        
+        public bool LoadScene(string sceneName){
+            if (!Application.CanStreamedLevelBeLoaded(sceneName)) { // Check if scene exists
+                Debug.LogError("Scene " + sceneName + " does not exist");
+                return false;
+            }
+            
+            UnityEngine.SceneManagement.SceneManager.LoadScene(sceneName);
+            return true;
+        }
     }
 }
