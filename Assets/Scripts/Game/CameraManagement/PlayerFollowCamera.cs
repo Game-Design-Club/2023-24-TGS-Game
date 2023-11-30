89f0f8b5c0a88a02f3ab67ba8d56f138f38e489f
TodTheSushiMonster <nconzetti@gmail.com>
2023-11-29 18:20:58 -0800

    Update PlayerFollowCamera.cs
    Fixed namespace for player follow camera and snapped location to player after the new level is loaded

--- Assets/Scripts/Game/CameraManagement/PlayerFollowCamera.cs
+++ Assets/Scripts/Game/CameraManagement/PlayerFollowCamera.cs
@@ -1,36 +1,36 @@
-using System;
-using System.Collections;
-using System.Collections.Generic;
-
 using Cinemachine;
 
 using Game.GameManagement;
-using Game.Player;
 
 using UnityEngine;
 
-public class PlayerFollowCamera : MonoBehaviour {
-    private CinemachineVirtualCamera _virtualCamera;
+namespace Game.CameraManagement
+{
+    public class PlayerFollowCamera : MonoBehaviour {
+        private CinemachineVirtualCamera _virtualCamera;
     
-    private void Awake() {
-        _virtualCamera = GetComponent<CinemachineVirtualCamera>();
-    }
+        private void Awake() {
+            _virtualCamera = GetComponent<CinemachineVirtualCamera>();
+        }
 
-    private void OnEnable() {
-        GameManager.Instance.OnLevelStart += OnLevelStart;
-        GameManager.Instance.OnLevelOver += OnLevelOver;
-    }
+        private void OnEnable() {
+            GameManager.Instance.OnLevelStart += OnLevelStart;
+            GameManager.Instance.OnLevelOver += OnLevelOver;
+        }
     
-    private void OnDisable() {
-        GameManager.Instance.OnLevelStart -= OnLevelStart;
-        GameManager.Instance.OnLevelOver -= OnLevelOver;
-    }
+        private void OnDisable() {
+            GameManager.Instance.OnLevelStart -= OnLevelStart;
+            GameManager.Instance.OnLevelOver -= OnLevelOver;
+        }
     
-    private void OnLevelStart() {
-        _virtualCamera.Follow = Player.Instance.transform;
-    }
+        private void OnLevelStart() {
+            Transform playerTransform = Player.Player.Instance.transform; // (Player.Player) because of namespace conflict
+            _virtualCamera.Follow = playerTransform;
+            _virtualCamera.ForceCameraPosition(playerTransform.position, transform.rotation);
+        }
     
-    private void OnLevelOver() {
-        _virtualCamera.Follow = null;
+        private void OnLevelOver() {
+            _virtualCamera.Follow = null;
+        }
     }
 }
