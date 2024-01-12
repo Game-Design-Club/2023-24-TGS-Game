557da6012206b9019c7b650d0f266b6c444b1d81
FragrantWheel32 <howlettnico@gmail.com>
2024-01-12 10:40:26 -0800

    Changed Storage and UI
    Prefabs are now stored in a scriptable object and you can now move objects around in the ui, and delete them

--- Assets/Scripts/Game/GameManagement/LevelManagement/LevelEditor.cs
+++ Assets/Scripts/Game/GameManagement/LevelManagement/LevelEditor.cs
@@ -1,5 +1,8 @@
+using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 using UnityEditor;
+using UnityEditorInternal;
 using UnityEngine;
 using UnityEngine.InputSystem;
 using UnityEngine.UIElements;
@@ -26,12 +29,9 @@ namespace Game.GameManagement.LevelManagement
         public class ObjectSelectionWindow : EditorWindow
         {
             // Add any properties or methods for your custom window here
-            private GameObject[] _prefabList;
+            private ObjectPrefabList _objectPrefabList;
             private Vector2 _scrollPosition;
-
-            private int _selectedPrefabIndex = 0;
-            private Rect[] _prefabRects;
-
+            
             [MenuItem("Window/Custom Popup Window")]
             public static void ShowWindow()
             {
@@ -44,13 +44,12 @@ namespace Game.GameManagement.LevelManagement
             {
                 // Initialize your prefab list here
                 LoadPrefabList();
-                _prefabRects = new Rect[_prefabList.Length];
             }
 
             private void OnGUI()
             {
                 // Add your GUI elements for the custom window here
-                // GUILayout.Label("Level Objects:");
+                GUILayout.Label("Level Objects:");
 
                 Event e = Event.current;
 
@@ -80,36 +79,54 @@ namespace Game.GameManagement.LevelManagement
                 // Display a preview, name, and button for each prefab in the list
                 _scrollPosition = EditorGUILayout.BeginScrollView(_scrollPosition);
 
+                
                 EditorGUILayout.BeginVertical();
-                for (int i = 0; i < _prefabList.Length; i++)
+                
+                for (int i = 0; i < _objectPrefabList.prefabList.Length; i++)
                 {
                     GUILayout.BeginHorizontal();
 
                     // Display the preview
-                    Texture2D previewTexture = AssetPreview.GetAssetPreview(_prefabList[i]);
+                    Texture2D previewTexture = AssetPreview.GetAssetPreview(_objectPrefabList.prefabList[i]);
                     if (previewTexture != null)
                     {
                         GUILayout.Label(previewTexture, GUILayout.MaxHeight(50), GUILayout.MaxWidth(50));
                     }
 
                     GUILayout.BeginVertical(); // Nested vertical layout for name and button
-
+                    GUILayout.BeginHorizontal();
                     // Display the name of the prefab
-                    GUILayout.Label(_prefabList[i].name, GUILayout.Width(100));
+                    GUILayout.Label(_objectPrefabList.prefabList[i].name, GUILayout.Width(100));
+                    GUILayout.FlexibleSpace();
+                    if (GUILayout.Button("delete", GUILayout.Width(50)))
+                    {
+                        RemovePrefabFromList(i);
+                    }
+                    if (i != 0 && GUILayout.Button("Ʌ", GUILayout.Width(20)))
+                    {
+                        (_objectPrefabList.prefabList[i - 1], _objectPrefabList.prefabList[i]) = (_objectPrefabList.prefabList[i], _objectPrefabList.prefabList[i - 1]);
+                    }
+                    GUILayout.EndHorizontal();
 
+                    GUILayout.BeginHorizontal();
                     // Add a button under the name
                     if (GUILayout.Button("Add", GUILayout.Width(60)))
                     {
-                        GameObject newObject = Instantiate(_prefabList[i], _currentLevel.transform);
-                        newObject.name = _prefabList[i].name;
+                        GameObject newObject = Instantiate(_objectPrefabList.prefabList[i], _currentLevel.transform);
+                        newObject.name = _objectPrefabList.prefabList[i].name;
                     }
+                    GUILayout.FlexibleSpace();
+                    if (i != _objectPrefabList.prefabList.Length - 1 && GUILayout.Button("V", GUILayout.Width(20)))
+                    {
+                        (_objectPrefabList.prefabList[i + 1], _objectPrefabList.prefabList[i]) = (_objectPrefabList.prefabList[i], _objectPrefabList.prefabList[i + 1]);
+                    }
+                    GUILayout.EndHorizontal();
 
                     GUILayout.EndVertical(); // End of nested vertical layout
 
                     GUILayout.EndHorizontal();
                     
                     // Assign the reordering rectangle
-                    _prefabRects[i] = GUILayoutUtility.GetLastRect();
                 }
 
                 EditorGUILayout.EndVertical();
@@ -117,7 +134,6 @@ namespace Game.GameManagement.LevelManagement
                 EditorGUILayout.EndScrollView();
 
                 // Handle prefab list reordering by dragging
-                HandlePrefabReordering();
                 
                 if (GUILayout.Button("Close"))
                 {
@@ -125,139 +141,43 @@ namespace Game.GameManagement.LevelManagement
                 }
             }
 
-            private void HandlePrefabReordering()
-            {
-                Event e = Event.current;
-                switch (e.type)
-                {
-                    case EventType.MouseDown:
-                        for (int i = 0; i < _prefabRects.Length; i++)
-                        {
-                            if (_prefabRects[i].Contains(e.mousePosition))
-                            {
-                                _selectedPrefabIndex = i;
-                                break;
-                            }
-                        }
-
-                        break;
-
-                    case EventType.MouseDrag:
-                        if (e.button == 0 && _prefabRects[_selectedPrefabIndex].Contains(e.mousePosition))
-                        {
-                            DragAndDrop.PrepareStartDrag();
-                            DragAndDrop.SetGenericData("PrefabDrag", _prefabList[_selectedPrefabIndex]);
-                            DragAndDrop.StartDrag(_prefabList[_selectedPrefabIndex].name);
-                            e.Use();
-                        }
-
-                        break;
-
-                    case EventType.MouseUp:
-                        if (DragAndDrop.GetGenericData("PrefabDrag") != null)
-                        {
-                            GameObject draggedPrefab = DragAndDrop.GetGenericData("PrefabDrag") as GameObject;
-
-                            if (draggedPrefab != null)
-                            {
-                                int draggedIndex = System.Array.IndexOf(_prefabList, draggedPrefab);
-                                int targetIndex = -1;
-
-                                for (int i = 0; i < _prefabRects.Length; i++)
-                                {
-                                    if (_prefabRects[i].Contains(e.mousePosition))
-                                    {
-                                        targetIndex = i;
-                                        break;
-                                    }
-                                }
-
-                                if (targetIndex != -1 && draggedIndex != targetIndex)
-                                {
-                                    // Reorder the prefab list
-                                    (_prefabList[draggedIndex], _prefabList[targetIndex]) = (_prefabList[targetIndex], _prefabList[draggedIndex]);
-
-                                    _selectedPrefabIndex = targetIndex;
-                                }
-                                DragAndDrop.SetGenericData("PrefabDrag", null);
-                                SavePrefabListToFile();
-                            }
-                        }
-
-                        break;
-                    case EventType.Repaint:
-                        if (DragAndDrop.GetGenericData("PrefabDrag") != null)
-                        {
-                            GUIStyle draggedStyle = new GUIStyle(GUI.skin.box);
-                            draggedStyle.normal.background = EditorGUIUtility.Load("builtin skins/darkskin/images/moveareatarget.png") as Texture2D;
-
-                            Rect r = _prefabRects[_selectedPrefabIndex];
-                            GUI.Box(r, GUIContent.none, draggedStyle);
-                        }
-                        break;
-                }
-            }
-            
             private void LoadPrefabList()
             {
-                string filePath = "Assets/Scripts/Game/GameManagement/LevelManagement/SavedPrefabs.txt";
+                string scriptableObjectPath = "Assets/Scripts/Game/GameManagement/LevelManagement/ObjectPrefabList.asset";
 
-                if (File.Exists(filePath))
-                {
-                    string[] savedPrefabPaths = File.ReadAllLines(filePath);
+                _objectPrefabList = AssetDatabase.LoadAssetAtPath<ObjectPrefabList>(scriptableObjectPath);
 
-                    // Initialize the prefab list
-                    _prefabList = new GameObject[savedPrefabPaths.Length];
-
-                    for (int i = 0; i < savedPrefabPaths.Length; i++)
-                    {
-                        _prefabList[i] = AssetDatabase.LoadAssetAtPath<GameObject>(savedPrefabPaths[i]);
-                    }
-                }
-                else
-                {
-                    Debug.LogError("SavedPrefabs.txt not found. Please create the file and add prefab paths.");
-                    _prefabList = new GameObject[0]; // Initialize an empty list
-                }
             }
 
             private void AddPrefabToList(GameObject prefab)
             {
                 // Check if the prefab is already in the list
-                if (System.Array.Exists(_prefabList, element => element == prefab))
+                if (System.Array.Exists(_objectPrefabList.prefabList, element => element == prefab))
                 {
                     Debug.LogWarning("Prefab is already in the list.");
                     return;
                 }
 
                 // Resize the prefab list array
-                System.Array.Resize(ref _prefabList, _prefabList.Length + 1);
-                System.Array.Resize(ref _prefabRects, _prefabRects.Length + 1);
+                System.Array.Resize(ref _objectPrefabList.prefabList, _objectPrefabList.prefabList.Length + 1);
 
                 // Add the new prefab to the list
-                _prefabList[_prefabList.Length - 1] = prefab;
+                _objectPrefabList.prefabList[^1] = prefab;
 
-                SavePrefabListToFile();
+                // Repaint the window to reflect the changes
+                Repaint();
+            }
+            
+            private void RemovePrefabFromList(int index)
+            {
+                List<GameObject> list = _objectPrefabList.prefabList.ToList();
+                list.RemoveAt(index);
+                _objectPrefabList.prefabList = list.ToArray();
 
                 // Repaint the window to reflect the changes
                 Repaint();
             }
 
-            private void SavePrefabListToFile()
-            {
-                string filePath = "Assets/Scripts/Game/GameManagement/LevelManagement/SavedPrefabs.txt";
-
-                // Create or overwrite the text file
-                using (StreamWriter writer = new StreamWriter(filePath, false))
-                {
-                    foreach (GameObject prefab in _prefabList)
-                    {
-                        string prefabPath = AssetDatabase.GetAssetPath(prefab);
-                        writer.WriteLine(prefabPath);
-                    }
-                }
-
-            }
         }
     }
 }
