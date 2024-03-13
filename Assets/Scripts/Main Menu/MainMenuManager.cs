ef6ecb8ba8068acfa121111f4b94288d9e5729fa
TodTheSushiMonster <nconzetti@gmail.com>
2024-03-12 21:58:33 -0700

    Adaptive UI for different resolutions
    Canvas now scales with screen size. Also started adding frameworks for turning on/off music and sfx

--- Assets/Scripts/Main Menu/MainMenuManager.cs
+++ Assets/Scripts/Main Menu/MainMenuManager.cs
@@ -9,30 +9,45 @@ namespace Main_Menu {
         private bool _freeze = false;
         
         public void StartGame() {
-            if (_freeze) {
-                return;
-            }
+            if (_freeze) return;
             _freeze = true;
             App.Instance.sceneManager.LoadScene(SceneConstants.Game);
         }
 
-        public void ShowCredits() {
-            if (!_freeze) {
-                _freeze = true;
-                App.Instance.sceneManager.LoadScene(SceneConstants.Credits);
-            }
+        public void ShowCredits() { 
+            if (_freeze) return;
+            
+            _freeze = true;
+            App.Instance.sceneManager.LoadScene(SceneConstants.Credits);
         }
 
         public void QuitGame() {
-            if (_freeze) {
-                return;
-            }
+            if (_freeze) return;
             _freeze = true;
+            
             if (Application.isEditor) {
                 UnityEditor.EditorApplication.isPlaying = false;
                 return;
             }
             Application.Quit();
+        }
+        
+        public void EraseProgress() {
+            if (_freeze) return;
+            
+            Debug.Log("Progress erased");
+        }
+        
+        public void SetSFXToggle(bool value) {
+            if (_freeze) return;
+            
+            App.Instance.audioManager.sfx.sfxVolume = value ? 1 : 0;
+        }
+        
+        public void SetMusicToggle(bool value) {
+            if (_freeze) return;
+            
+            App.Instance.audioManager.music.musicVolume = value ? 1 : 0;
         }
     }
 }
