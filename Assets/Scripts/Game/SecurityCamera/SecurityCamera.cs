c309dce0be2fe845b100c37a726ea0491a50226c
TodTheSushiMonster <nconzetti@gmail.com>
2024-04-09 22:44:04 -0700

    Rotation start setup
    Not quite working yet but almost there

--- Assets/Scripts/Game/SecurityCamera/SecurityCamera.cs
+++ Assets/Scripts/Game/SecurityCamera/SecurityCamera.cs
@@ -1,5 +1,7 @@
+using System;
 using System.Collections;
 
+using Game.GameManagement;
 using Game.NightLevels.Shooter;
 using Game.PlayerComponents;
 
@@ -10,6 +12,13 @@ namespace Game.SecurityCamera {
     public class SecurityCamera : MonoBehaviour {
         [SerializeField] private Transform shootPoint;
         [SerializeField] private GameObject bulletPrefab;
+        
+        [SerializeField] private bool rotate = true;
+        [SerializeField] private float rotateLeft = 45;
+        [SerializeField] private float rotateRight = 45;
+        [SerializeField] private float rotationSpeed = 1f;
+        [SerializeField] private Transform rotationPoint;
+        
         [SerializeField] private float shootInterval = 1f;
         [SerializeField] private Light2D shootingSignalLight;
         [SerializeField] private SpriteRenderer shootingSignal;
@@ -17,14 +26,35 @@ namespace Game.SecurityCamera {
         [SerializeField] private Color idleColor = Color.green;
         
         private Vector2 DirectionToPlayer => (Player.Instance.transform.position - transform.position).normalized;
+        private float _baseRotation;
+        private float _adjustedRotationLeft;
+        private float _adjustedRotationRight;
         
         // Unity functions
         private void Start() {
             shootingSignal.color = idleColor;
             shootingSignalLight.color = idleColor;
+            _baseRotation = rotationPoint.rotation.eulerAngles.z;
+            _adjustedRotationLeft = (_baseRotation + rotateLeft) % 360;
+            _adjustedRotationRight = (_baseRotation - rotateRight + 360) % 360;
+            
+        }
+
+        private void OnEnable() {
+            GameManagerEvents.OnLevelStart += OnLevelStart;
         }
         
+        private void OnDisable() {
+            GameManagerEvents.OnLevelStart -= OnLevelStart;
+        }
+
         // Private functions
+        private void OnLevelStart() {
+            if (rotate) {
+                StartCoroutine(Rotate());
+            }
+        }
+        
         private IEnumerator ShootPlayer() {
             while (true) {
                 GameObject bullet = Instantiate(bulletPrefab, shootPoint.position, Quaternion.identity);
@@ -33,17 +63,43 @@ namespace Game.SecurityCamera {
             }
         }
         
+        private IEnumerator FacePlayer() {
+            while (true) {
+                Vector2 direction = DirectionToPlayer;
+                float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+                rotationPoint.rotation = Quaternion.Euler(0, 0, angle - 90);
+                yield return new WaitForFixedUpdate();
+            }
+        }
+        
         // Internal functions
         internal void StartShooting() {
+            StopAllCoroutines();
             StartCoroutine(ShootPlayer());
+            StartCoroutine(FacePlayer());
             shootingSignal.color = shootingColor;
             shootingSignalLight.color = shootingColor;
         }
         
         internal void StopShooting() {
-            StopAllCoroutines();
             shootingSignal.color = idleColor;
             shootingSignalLight.color = idleColor;
+            StopAllCoroutines();
+            StartCoroutine(Rotate());
+        }
+
+        private IEnumerator Rotate() {
+            while (true) {
+                rotationPoint.Rotate(Vector3.forward, rotationSpeed);
+                yield return new WaitForFixedUpdate();
+
+                float currentAngle = rotationPoint.rotation.eulerAngles.z;
+                if (currentAngle > _adjustedRotationLeft && rotationSpeed < 0) {
+                    rotationSpeed = -rotationSpeed;
+                } else if (currentAngle < _adjustedRotationRight && rotationSpeed > 0) {
+                    rotationSpeed = -rotationSpeed;
+                }
+            }
         }
     }
 }
