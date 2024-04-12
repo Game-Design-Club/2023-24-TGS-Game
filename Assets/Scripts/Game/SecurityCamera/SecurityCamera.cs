3ab3fe45a45f3d6023e8cff9bbfa68083a78082f
TodTheSushiMonster <nconzetti@gmail.com>
2024-04-11 20:39:53 -0700

    Redoing rotating system

--- Assets/Scripts/Game/SecurityCamera/SecurityCamera.cs
+++ Assets/Scripts/Game/SecurityCamera/SecurityCamera.cs
@@ -1,4 +1,3 @@
-using System;
 using System.Collections;
 
 using Game.GameManagement;
@@ -14,9 +13,11 @@ namespace Game.SecurityCamera {
         [SerializeField] private GameObject bulletPrefab;
         
         [SerializeField] private bool rotate = true;
-        [SerializeField] private float rotateLeft = 45;
-        [SerializeField] private float rotateRight = 45;
+        [SerializeField] private bool rotateClockwiseFirst = true;
+        [SerializeField] private float rotateCounterClockwise = 45;
+        [SerializeField] private float rotateClockwise = 45;
         [SerializeField] private float rotationSpeed = 1f;
+        [SerializeField] private float playerLockRotationSpeed = 2f;
         [SerializeField] private Transform rotationPoint;
         
         [SerializeField] private float shootInterval = 1f;
@@ -26,18 +27,19 @@ namespace Game.SecurityCamera {
         [SerializeField] private Color idleColor = Color.green;
         
         private Vector2 DirectionToPlayer => (Player.Instance.transform.position - transform.position).normalized;
+        private bool _isShooting;
+
         private float _baseRotation;
-        private float _adjustedRotationLeft;
-        private float _adjustedRotationRight;
+        private float _currentRotation;
+        private int _rotateDirection;
         
         // Unity functions
         private void Start() {
             shootingSignal.color = idleColor;
             shootingSignalLight.color = idleColor;
-            _baseRotation = rotationPoint.rotation.eulerAngles.z;
-            _adjustedRotationLeft = (_baseRotation + rotateLeft) % 360;
-            _adjustedRotationRight = (_baseRotation - rotateRight + 360) % 360;
             
+            rotateClockwise *= -1;
+            _rotateDirection = rotateClockwiseFirst ? -1 : 1;
         }
 
         private void OnEnable() {
@@ -47,59 +49,52 @@ namespace Game.SecurityCamera {
         private void OnDisable() {
             GameManagerEvents.OnLevelStart -= OnLevelStart;
         }
+        
+        private void FixedUpdate() {
+            Rotate();
+        }
 
         // Private functions
         private void OnLevelStart() {
-            if (rotate) {
-                StartCoroutine(Rotate());
-            }
+            StartCoroutine(ShootPlayer());
         }
         
         private IEnumerator ShootPlayer() {
             while (true) {
+                yield return new WaitUntil(() => _isShooting);
                 GameObject bullet = Instantiate(bulletPrefab, shootPoint.position, Quaternion.identity);
                 bullet.GetComponent<Bullet>().Shoot(DirectionToPlayer, gameObject);
                 yield return new WaitForSeconds(shootInterval);
             }
         }
-        
-        private IEnumerator FacePlayer() {
-            while (true) {
-                Vector2 direction = DirectionToPlayer;
-                float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
-                rotationPoint.rotation = Quaternion.Euler(0, 0, angle - 90);
-                yield return new WaitForFixedUpdate();
+
+        private void Rotate() {
+            if (!rotate) return;
+
+            float rotationThisFrame = rotationSpeed * _rotateDirection;
+            _currentRotation += rotationThisFrame;
+
+            if (_rotateDirection > 0 && _currentRotation > rotateCounterClockwise) {
+                _rotateDirection *= -1;
+                // Flip to clockwise
+            } else if (_rotateDirection < 0 && _currentRotation < rotateClockwise) {
+                _rotateDirection *= -1;
+                // Flip to counterclockwise
             }
+            rotationPoint.rotation = Quaternion.Euler(new (0,0, (_currentRotation + _baseRotation + 360) % 360));
         }
         
         // Internal functions
         internal void StartShooting() {
-            StopAllCoroutines();
-            StartCoroutine(ShootPlayer());
-            StartCoroutine(FacePlayer());
             shootingSignal.color = shootingColor;
             shootingSignalLight.color = shootingColor;
+            _isShooting = true;
         }
         
         internal void StopShooting() {
             shootingSignal.color = idleColor;
             shootingSignalLight.color = idleColor;
-            StopAllCoroutines();
-            StartCoroutine(Rotate());
-        }
-
-        private IEnumerator Rotate() {
-            while (true) {
-                rotationPoint.Rotate(Vector3.forward, rotationSpeed);
-                yield return new WaitForFixedUpdate();
-
-                float currentAngle = rotationPoint.rotation.eulerAngles.z;
-                if (currentAngle > _adjustedRotationLeft && rotationSpeed < 0) {
-                    rotationSpeed = -rotationSpeed;
-                } else if (currentAngle < _adjustedRotationRight && rotationSpeed > 0) {
-                    rotationSpeed = -rotationSpeed;
-                }
-            }
+            _isShooting = false;
         }
     }
 }
