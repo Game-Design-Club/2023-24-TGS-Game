adc5cecd8e252366ff36f2fb2ad681f0e375a8dc
TodTheSushiMonster <nconzetti@gmail.com>
2024-05-13 18:05:50 -0700

    Meta updates + tiny script improvements

--- Assets/Scripts/Game/Interactables/Door.cs
+++ Assets/Scripts/Game/Interactables/Door.cs
@@ -8,14 +8,14 @@ namespace Game.Interactables
         [SerializeField] private SpriteRenderer spriteRenderer;
         [SerializeField] private float closedOpacity = .3f;
         [SerializeField] private ShadowCaster2D shadowCaster2D;
-        [SerializeField] private Collider2D collider2D;
+        [SerializeField] private Collider2D collider;
         [SerializeField] private Light2D light2D;
         
         public void Opened() {
             Color c = spriteRenderer.color;
             spriteRenderer.color = new Color(c.r, c.g, c.b, closedOpacity);
             shadowCaster2D.enabled = false;
-            collider2D.enabled = false;
+            collider.enabled = false;
             light2D.enabled = false;
         }
     }
