a38c94bb56ee20745824960d513a685ee07160c1
TodTheSushiMonster <nconzetti@gmail.com>
2024-05-16 23:22:38 -0700

    Final updates + move between fix

--- Assets/Scripts/Game/MovingObjects/MoveBetween.cs
+++ Assets/Scripts/Game/MovingObjects/MoveBetween.cs
@@ -130,10 +130,11 @@ namespace Game.MovingObjects
                 if (loop == LoopType.Cyclical && _points.Length > 2) { // If there are only 2 points, it's a straight line
                     _lineRenderer.loop = true;
                 }
-                Vector3[] lineRendererPoints = new Vector3[_points.Length + 1];
+                Vector3[] lineRendererPoints = new Vector3[_points.Length];
                 for (int i = 0; i < _points.Length; i++) {
                     lineRendererPoints[i] = new Vector3(_points[i].x, _points[i].y, 0);
                 }
+                _lineRenderer.positionCount = _points.Length;
                 _lineRenderer.SetPositions(lineRendererPoints);
             }
 
