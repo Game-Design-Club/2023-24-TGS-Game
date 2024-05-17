9d88a66f8c9b94fd1f1cf73a73b5eeb1175f2e8d
FragrantWheel32 <howlettnico@gmail.com>
2024-05-16 18:45:48 -0700

    robot paths can now go on half blocks

--- Assets/Scripts/Game/Robots/RobotPath.cs
+++ Assets/Scripts/Game/Robots/RobotPath.cs
@@ -55,7 +55,7 @@ namespace Game.Robots
 
         public void MovePoint(int index, Vector2 position)
         {
-            points[index].position = new Vector2(Mathf.RoundToInt(position.x * 2) / 2, Mathf.RoundToInt(position.y * 2) / 2);
+            points[index].position = new Vector2(Mathf.RoundToInt(position.x * 2) / 2f, Mathf.RoundToInt(position.y * 2) / 2f);
             CalculateLength();
         }
 
